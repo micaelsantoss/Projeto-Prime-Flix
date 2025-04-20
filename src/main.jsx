f8fbfd63d99fc527cd0ccb8b07bacf9e23816fd1
Micael Santos <154888086+micaelsantoss@users.noreply.github.com>
2025-04-20 11:51:31 -0300

    removido swiper/element/bundle e ajustado para swiper/react

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
-import { register } from 'swiper/element/bundle'
-
-register();
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
